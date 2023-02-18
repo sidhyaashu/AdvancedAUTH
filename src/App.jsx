2e5d79f51b023fa994a6e3fa7f5982dcb4642097
asutoshsidhya <ashutoshsidhya69@gmail.com>
2023-02-18 13:48:01 +0530

    auth

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Register from './register/Register'
+
+const App = () => {
+  return (
+    <main className='app'>
+      <Register/>
+    </main>
+  )
+}
+
+export default App
